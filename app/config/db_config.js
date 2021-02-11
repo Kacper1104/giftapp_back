cf696353a4356581a9bb03eafbfeaaf28a58251d
Kacper Kozimor <37913081+Kacper1104@users.noreply.github.com>
2021-02-11 15:11:57 +0100

    Update db_config.js

--- app/config/db_config.js
+++ app/config/db_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "eu-cdbr-west-03.cleardb.net/",
+  HOST: "eu-cdbr-west-03.cleardb.net",
   USER: "bee31fe8be8685",
   PASSWORD: "98c46258",
   DB: "heroku_9378fb7a7bd93cc"
