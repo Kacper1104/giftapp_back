4914473ad1f5057652263c710284b5f691f9fce4
Kacper Kozimor <37913081+Kacper1104@users.noreply.github.com>
2021-02-11 15:09:25 +0100

    Update db_config.js

--- app/config/db_config.js
+++ app/config/db_config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "root",
-  DB: "giftapp"
+  HOST: "eu-cdbr-west-03.cleardb.net/",
+  USER: "bee31fe8be8685",
+  PASSWORD: "98c46258",
+  DB: "heroku_9378fb7a7bd93cc"
 };
