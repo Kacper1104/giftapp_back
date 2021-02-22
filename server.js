c0ad73949dcd63aa18f80f2a0ce76dfd1a606512
Kacper1104 <kacper.kozimor@gmail.com>
2021-02-22 21:26:31 +0100

    err blocked by client error

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
 
-app.use(cors()) // Use this after the variable declaration
+app.use(cors()); // Use this after the variable declaration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "production") {
+	// Exprees will serve up production assets
+	app.use(express.static("client/build"));
+
+	// Express serve up index.html file if it doesn't recognize route
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 require("./app/routes/auth.js")(app);
 require("./app/routes/user.js")(app);
@@ -15,5 +25,5 @@ require("./app/routes/reservation.js")(app);
 require("./app/routes/code.js")(app);
 
 app.listen(process.env.PORT || 6060, () => {
-  console.log("Server is running on port 6060.");
+	console.log("Server is running on port 6060.");
 });
