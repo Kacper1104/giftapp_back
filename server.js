1330011fb9e5dd2422dae3b4ccc4be2491354725
Kacper1104 <kacper.kozimor@gmail.com>
2021-02-23 00:52:06 +0100

    reverted commit

--- server.js
+++ server.js
@@ -7,16 +7,6 @@ app.use(cors()); // Use this after the variable declaration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === "production") {
-	// Exprees will serve up production assets
-	app.use(express.static("client/build"));
-
-	// Express serve up index.html file if it doesn't recognize route
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
-}
-
 require("./app/routes/auth.js")(app);
 require("./app/routes/user.js")(app);
 require("./app/routes/event.js")(app);
